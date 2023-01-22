b64ba5a64db33f3effc8adc7cbee20505b13a3f8
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-01-22 22:45:36 +0530

    feat: added terms page

---
+++ client/src/pages/terms.tsx
@@ -0,0 +1,10 @@
+import Container from "../components/Container";
+
+export default function Terms() {
+  document.title = "Terms and Conditions";
+  return (
+    <Container>
+      <h1 className="desktop:text-4xl text-2xl">Terms and Conditions</h1>
+    </Container>
+  );
+}
