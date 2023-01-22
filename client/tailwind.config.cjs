bbbee147525eed75e550cf25e343c627c6d2a77f
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-01-22 14:34:25 +0530

    build: update dep

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      desktop: "1024px",
+      md: "768px",
+      sm: "640px",
+      lg: "1024px",
+    },
     extend: {},
   },
   plugins: [],
