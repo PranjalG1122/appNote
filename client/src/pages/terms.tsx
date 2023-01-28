afe3a747cf4e9463f41deffa9f72e692c4d8679c
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-01-28 17:43:08 +0530

    fix: added 'read and modify' to general terms

--- client/src/pages/terms.tsx
+++ client/src/pages/terms.tsx
@@ -29,7 +29,9 @@ export default function Terms() {
             3. appNote is not responsible for any data stored with the service.
           </li>
         </ul>
-        <h1 className="desktop:text-3xl text-2xl font-semibold">General Terms</h1>
+        <h1 className="desktop:text-3xl text-2xl font-semibold">
+          General Terms
+        </h1>
         <ul className="text-xl">
           <li>
             1. appNote reserves the right to terminate any account for any
@@ -44,9 +46,13 @@ export default function Terms() {
             3. appNote reserves the right to refuse service to any user for any
             reason.
           </li>
+          <li>
+            4. appNote reserves the right to read and modify any data stored on
+            the server.
+          </li>
         </ul>
         <Link to="/">
-          <p className="text-white w-full bg-gradient-to-r from-blue-500 via-blue-600 to-indigo-600 hover:bg-gradient-to-br focus:outline-none shadow-sm shadow-blue-800/80 font-semibold rounded text-xl p-2 text-center">
+          <p className="mt-4 text-white w-full bg-gradient-to-r from-blue-500 via-blue-600 to-indigo-600 hover:bg-gradient-to-br focus:outline-none shadow-sm shadow-blue-800/80 font-semibold rounded text-xl p-2 text-center">
             Back to Home
           </p>
         </Link>
